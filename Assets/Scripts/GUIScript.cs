c8122a5fa096d64f3db48874fae4b2eda4ea1d2c
Michael Bo Magling <themikker@hotmail.com>
2013-04-19 12:16:54 +0200

    Map GUI
    Implemented map GUI

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -7,8 +7,9 @@ public class GUIScript : MonoBehaviour {
 	public static long SCORE;
 	
     public GUISkin gSkin;
-    public Texture2D runningSoldiers, runningGoal, damagebar, swordLeft, swordRight, bloodsplatter;
-
+    public Texture2D runningSoldiers1, runningSoldiers2, runningHero1, runningHero2, runningGoal, damagebar, swordLeft, swordRight, bloodsplatter;
+    public Texture2D backgroundScrollScore,backgroundScrollLeft,backgroundScrollMid,backgroundScrollRight;
+    public Vector2 scrollScoreOffset, scrollLeftOffset, scrollMidOffset, scrollRightOffset;
     public float engagePercent, releasePercent;
     public float fixedEngagePercent;
     public bool engageFixed, releaseFixed;
@@ -21,19 +22,26 @@ public class GUIScript : MonoBehaviour {
 
 	private float currentZ;
 	private float armyZ;
-	private float minZ = 0;
+	private float minZ = -50;
 	private float maxZ;
 
+    private float soldierAnim;
     
 
 	// Use this for initialization
 	void Start () {
 		maxZ = (LevelCreator.LengthConverter(LevelCreator.LEVEL_LENGTH)*64)-32;
         HitList = new List<HitAccuracy>();
+        soldierAnim = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        soldierAnim += Time.deltaTime;
+
+        if (soldierAnim > 1)
+            soldierAnim -= 1;
+
 		currentZ = ObstacleController.PLAYER.transform.position.z;
 		armyZ = ObstacleController.ARMY.transform.position.z;
 
@@ -57,19 +65,44 @@ public class GUIScript : MonoBehaviour {
     void Level_Interface()
     {
         GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
-        GUI.Box(new Rect(Screen.width-225, 25, 200, 75), "");
-        GUI.Label(new Rect(Screen.width - 225 + 15, 25 + 15, 200, 75), "Score: "+ SCORE);
-        GUI.Label(new Rect(Screen.width - 225 + 15, 25 + 15*2, 200, 75), "Multiplier: x2");
-
-        GUI.Box(new Rect(15, Screen.height - 65, Screen.width - 30, 50), "");
-        GUI.DrawTexture(new Rect(15+currentZ/(maxZ-minZ)*Screen.width-50, Screen.height - 64, 50, 50), runningSoldiers);
-        GUI.DrawTexture(new Rect(15+armyZ/(maxZ-minZ)*Screen.width-50, Screen.height - 64, 50, 50), runningSoldiers);
-        GUI.DrawTexture(new Rect(Screen.width-65, Screen.height - 64, 50, 50), runningSoldiers);
+        if (backgroundScrollScore != null)
+        {
+            GUI.DrawTexture(new Rect(Screen.width - 225 + scrollScoreOffset.x, 25 + scrollScoreOffset.y, backgroundScrollScore.width, backgroundScrollScore.height), backgroundScrollScore);
+        }
+        //GUI.Box(new Rect(Screen.width-225, 25, 200, 75), "");
+        GUI.color = Color.black;
+        GUI.Label(new Rect(Screen.width - 225 + 15, 25 + 15-5, 200, 75), "Score:   "+ SCORE);
+        GUI.Label(new Rect(Screen.width - 225 + 15, 25 + 15*2, 200, 75), "Multiplier:   x2");
+        GUI.color = Color.white;
+        if (backgroundScrollLeft != null)
+        {
+            GUI.DrawTexture(new Rect(15 + scrollLeftOffset.x, Screen.height - 65 + scrollLeftOffset.y, backgroundScrollLeft.width, backgroundScrollLeft.height), backgroundScrollLeft);
+        }
+        if (backgroundScrollRight != null)
+        {
+            GUI.DrawTexture(new Rect(Screen.width - 30 + scrollRightOffset.x, Screen.height - 65 + scrollRightOffset.y, backgroundScrollRight.width, backgroundScrollRight.height), backgroundScrollRight);
+        }
+        if (backgroundScrollMid != null)
+        {
+            GUI.DrawTexture(new Rect(15 + scrollMidOffset.x, Screen.height - 65 + scrollMidOffset.y, Screen.width - 30 - scrollMidOffset.x*2+1, backgroundScrollMid.height), backgroundScrollMid);
+        }
 
         GUI.DrawTexture(new Rect(Screen.width / 2 - 250, Screen.height - 90, 500, 12), damagebar);
-     //   GUI.DrawTexture(new Rect(Screen.width / 2 - 250 - swordLeft.width / 2, Screen.height - 90 - swordLeft.height / 2 + 6, swordLeft.width, swordLeft.height), swordLeft);
-    //    GUI.DrawTexture(new Rect(Screen.width / 2 + 250 - swordRight.width / 2, Screen.height - 90 - swordRight.height / 2 + 6, swordRight.width, swordRight.height), swordRight);
-        GUI.DrawTexture(new Rect(Screen.width / 2 - bloodsplatter.width / 2, Screen.height - 90 - bloodsplatter.height / 2 + 6, bloodsplatter.width, bloodsplatter.height), bloodsplatter);
+        //   GUI.DrawTexture(new Rect(Screen.width / 2 - 250 - swordLeft.width / 2, Screen.height - 90 - swordLeft.height / 2 + 6, swordLeft.width, swordLeft.height), swordLeft);
+        //    GUI.DrawTexture(new Rect(Screen.width / 2 + 250 - swordRight.width / 2, Screen.height - 90 - swordRight.height / 2 + 6, swordRight.width, swordRight.height), swordRight);
+        //GUI.DrawTexture(new Rect(Screen.width / 2 - bloodsplatter.width / 2, Screen.height - 90 - bloodsplatter.height / 2 + 6, bloodsplatter.width, bloodsplatter.height), bloodsplatter);
+        GUI.EndGroup();
+
+        GUI.BeginGroup(new Rect(15 + scrollMidOffset.x, Screen.height - 64 - 6, Screen.width - 30 - scrollMidOffset.x * 2 + 1, Screen.height));
+        //GUI.Box(new Rect(15, Screen.height - 65, Screen.width - 30, 50), "");
+        GUI.DrawTexture(new Rect(currentZ / (maxZ - minZ) * (Screen.width - 30 - scrollMidOffset.x * 2), 0, 128, 64), runningSoldiers1);
+       
+        if (soldierAnim > 0.5)
+            GUI.DrawTexture(new Rect(armyZ / (maxZ - minZ) * (Screen.width - 30 - scrollMidOffset.x * 2), 0, 128, 64), runningSoldiers1);
+        else
+            GUI.DrawTexture(new Rect(armyZ / (maxZ - minZ) * (Screen.width - 30 - scrollMidOffset.x * 2), 0, 128, 64), runningSoldiers2);
+        //GUI.DrawTexture(new Rect(Screen.width - 30 - scrollMidOffset.x * 2 + 1 - 128, 0, 128, 64), runningSoldiers1);
+
         GUI.EndGroup();
     }
 
@@ -84,7 +117,7 @@ public class GUIScript : MonoBehaviour {
         else
         {
 
-            GUI.Label(new Rect(10, 50, 80, 30), "" + Time.timeSinceLevelLoad);
+            //GUI.Label(new Rect(10, 50, 80, 30), "" + Time.timeSinceLevelLoad);
             GUI.skin = gSkin;
             Level_Interface();
 
